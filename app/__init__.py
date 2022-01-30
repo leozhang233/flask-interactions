8babbce34f463194cdbcad1a4b835becbc8c82c9
leozhang233 <leo865843040@gmail.com>
2022-01-29 22:21:44 -0800

    new webapp

--- app/__init__.py
+++ app/__init__.py
@@ -1,115 +1,102 @@
 # to run this website and watch for changes: 
 # $ export FLASK_ENV=development; flask run
 
-
 from flask import Flask, g, render_template, request
-
-import sklearn as sk
-import matplotlib.pyplot as plt
-import numpy as np
-import pickle
-
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-
+import pandas as pd
+import sqlite3
 import io
 import base64
-
 
 # Create web app, run with flask run
 # (set "FLASK_ENV" variable to "development" first!!!)
 
 app = Flask(__name__)
 
-# Create main page (fancy)
-
 @app.route('/')
-
-# def main():
-#     return render_template("main.html")
-
-# comment out the below to focus on just the fundamentals
-
-# after running
-# $ export FLASK_ENV=development; flask run
-# site will be available at 
-# http://localhost:5000
-
+# Create main page (fancy)
 def main():
     return render_template('main_better.html')
 
 # Show url matching
-
-@app.route('/hello/')
-def hello():
-    return render_template('hello.html')
-
-@app.route('/hello/<name>/')
-def hello_name(name):
-    return render_template('hello.html', name=name)
-
 # Page with form
 
-@app.route('/ask/', methods=['POST', 'GET'])
-def ask():
-    if request.method == 'GET':
-        return render_template('ask.html')
-    else:
-        try:
-            return render_template('ask.html', name=request.form['name'], student=request.form['student'])
-        except:
-            return render_template('ask.html')
-
-# File uploads and interfacing with complex Python
-# basic version
-
-@app.route('/submit-basic/', methods=['POST', 'GET'])
-def submit_basic():
-    if request.method == 'GET':
-        return render_template('submit-basic.html')
-    else:
-        try:
-            return render_template('submit-basic.html', thanks = True)
-        except:
-            return render_template('submit-basic.html', error=True)
-
-# nontrivial version: makes a prediction and shows a viz
-@app.route('/submit-advanced/', methods=['POST', 'GET'])
+@app.route('/submit/', methods=['POST', 'GET'])
 def submit():
     if request.method == 'GET':
         return render_template('submit.html')
     else:
         try:
-            # retrieve the image
-            img = request.files['image']
-            img = np.loadtxt(img)
-            
-            # reshape into appropriate format for prediction
-            x = img.reshape(1, 64)
-            
-            # load up a pre-trained model and get a prediction
-            model = pickle.load(open("mnist-model/model.pkl", 'rb'))
-            d = model.predict(x)[0]
-
-            # plot the image itself
-            fig = Figure(figsize = (3, 3))
-            ax = fig.add_subplot(1, 1, 1,)
-            ax.imshow(img, cmap = "binary")
-            ax.axis("off")
-            
-            # in order to show the plot on flask, we need to do a few tricks
-            # Convert plot to PNG image
-            # need to: 
-            # import io 
-            # import base64 
-            pngImage = io.BytesIO()
-            FigureCanvas(fig).print_png(pngImage)
-            
-            # Encode PNG image to base64 string
-            pngImageB64String = "data:image/png;base64,"
-            pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
-            
-            # finally we can render the template with the prediction and image
-            return render_template('submit.html', digit=d, image=pngImageB64String)
+            insert_message(request)
+            return render_template('submit.html', thanks = True)
         except:
             return render_template('submit.html', error=True)
+
+def get_message_db():
+    # check whether there is a database called message_db in the 
+    # g attribute of the app
+    if 'message_db' not in g:
+        g.message_db = sqlite3.connect("messages_db.sqlite")
+
+    cursor = g.message_db.cursor()
+    # check whether a table called messages exists in message_db,
+    # and create it if not
+    cmd = \
+        """
+        CREATE TABLE IF NOT EXISTS messages (
+            id integer,
+            handle text,
+            message text);
+        """
+    cursor.execute(cmd)
+    # return the connection
+    return g.message_db
+
+def insert_message(request):
+    # access the database
+    g.message_db = get_message_db()
+    cursor = g.message_db.cursor()
+    # extract message and handle from request
+    message = request.form["message"]
+    handle = request.form["handle"]
+    
+    # ensure that the ID number of each message is unique
+    mycursor = cursor.execute('select * from messages;')
+    results = mycursor.fetchall()
+    id = len(results) + 1
+    
+    # insert id, message and handle to the database
+    cmd = \
+        """
+        INSERT INTO messages (id, handle, message)
+        VALUES ({id}, '{handle}', '{message}')
+        """.format(id = id, handle = handle, message = message)
+
+    cursor.execute(cmd)
+    # ensure that the row insertion has been saved
+    g.message_db.commit()
+    # close the connection
+    g.message_db.close()
+
+def random_messages(n):
+    # access the database
+    g.message_db = get_message_db()
+    # select 5 random messages or fewer if necessary
+    cmd = \
+        """
+        SELECT * FROM messages
+        ORDER BY RANDOM()
+        LIMIT {n}
+        """.format(n = n)
+
+    df = pd.read_sql_query(cmd, g.message_db)
+    # close the connection
+    g.message_db.close()
+    return df
+
+@app.route('/view/')
+def view():
+    try:
+        ran_messages = random_messages(5)
+        return render_template('view.html', ran_messages = ran_messages)
+    except:
+        return render_template('view.html', error=True)
